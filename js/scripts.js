3434465ddf6e0abca8bbe37c70e8b8eeb163091c
Michael Schennum <mikeschenphoto@gmail.com>
2016-01-29 09:07:33 -0800

    adds price function based on size

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,18 @@ function Pizza(pizzaSize) {
 	this.pizzaToppings = [];
 }
 
+Pizza.prototype.totalPrice = function() {
+	if (this.pizzaSize === "small") {
+		this.price = 10;
+	}
+	if (this.pizzaSize === "medium") {
+		this.price = 12;
+	}
+	if (this.pizzaSize === "large") {
+		this.price = 14;
+	}
+
+	return this.price;
+};
+
+//pepperoni, mushrooms, onions, sausage, bacon, extra cheese, olives, pineapple, spinach
--- specs/specs.js
+++ specs/specs.js
@@ -1,7 +1,12 @@
 describe('Pizza', function() {
 	it("creates a new pizza with the given toppings and size", function() {
-		var testPizza = Pizza("small");
+		var testPizza = new Pizza("small");
 		expect(testPizza.pizzaSize).to.equal("small");
 		expect(testPizza.pizzaToppings).to.eql([]);
+	});
+
+	it("returns price of pizza size", function() {
+		var testPizza = new Pizza("small");
+		expect(testPizza.totalPrice()).to.equal(10);
 	});
 });
