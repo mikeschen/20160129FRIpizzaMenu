0f4e755066b80576ea4a255dbf8471bf9f36832d
Michael Schennum <mikeschenphoto@gmail.com>
2016-01-29 08:36:11 -0800

    adds pizza object

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function Pizza(pizzaSize) {
+	this.pizzaSize = pizzaSize;
+	this.pizzaToppings = [];
+}
+
--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,7 @@
-
+describe('Pizza', function() {
+	it("creates a new pizza with the given toppings and size", function() {
+		var testPizza = Pizza("small");
+		expect(testPizza.pizzaSize).to.equal("small");
+		expect(testPizza.pizzaToppings).to.eql([]);
+	});
+});
